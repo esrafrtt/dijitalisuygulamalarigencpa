dd93c1a3a0b4f3d44d72e873d3c72f70be751ff7
esrafrtt <esrafrtt145@gmail.com>
2022-11-21 17:58:26 +0300

    upp

--- Qata-master/Entities/Concrete/Customer.cs
+++ Qata-master/Entities/Concrete/Customer.cs
@@ -14,7 +14,9 @@ namespace Entities.Concrete
         public int Id { get; set; }
         [Required(ErrorMessage = "Zorunlu Alan")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Zorunlu Alan")]
         public string PersonInCharge { get; set; }
+        [Required(ErrorMessage = "Zorunlu Alan")]
         public string PersonInChargeSurName { get; set; }
         public string KindOf { get; set; }
 
@@ -47,6 +49,7 @@ namespace Entities.Concrete
         public string Region { get; set; }
         //[MaxLength(11, ErrorMessage = "11 Karakterden fazla girilemez")]
         [RegularExpression("^([0-9]{10})$", ErrorMessage = "Format hatası")]
+        [Required(ErrorMessage = "Zorunlu Alan")]
         public string TaxAdministration { get; set; }
         public string TaxNumber { get; set; }
         //[MaxLength(11, ErrorMessage = "11 Karakterden fazla girilemez")]
